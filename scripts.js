b08beb14edc953b5f6af75fc0468dcbac20ae672
Vito Di Grigoli <73437231+vitodigrigoli@users.noreply.github.com>
2025-04-10 18:00:52 +0200

    update before restart coding

--- scripts.js
+++ scripts.js
@@ -1,41 +1,66 @@
-const scrollerInner = document.querySelector('.scroller__media__inner')
-const scrollerContent = Array.from(scrollerInner.children)
+gsap.registerPlugin(ScrollTrigger);
 
-scrollerContent.forEach( item  =>{
-		const duplicatedItem = item.cloneNode(true)
-		duplicatedItem.setAttribute('aria-hidden', true)
-		scrollerInner.appendChild(duplicatedItem)
-	}
-)
+const init = () =>{
 
-gsap.registerPlugin(ScrollTrigger)
-const splitTypes =  document.querySelectorAll('.text__content > *')
-splitTypes.forEach( (char, i) => {
-	const text = new SplitType(char, {types:'chars, words'})
+	const scrollerInner = document.querySelector('.scroller__media__inner')
+	const scrollerContent = Array.from(scrollerInner.children)
 	
-	gsap.from(text.chars, {
-		scrollTrigger: {
-			trigger: char,
-			scrub: true,
-			start: 'top 35%',
-			end: 'bottom 35%'
-		},
-		transformOrigin: 'top',
-		stagger: 0.1,
-		opacity: 0.2,
+	scrollerContent.forEach( item  =>{
+			const duplicatedItem = item.cloneNode(true)
+			duplicatedItem.setAttribute('aria-hidden', true)
+			scrollerInner.appendChild(duplicatedItem)
+		}
+	)
+	
+	const splitTypes =  document.querySelectorAll('.text__content > *')
+	splitTypes.forEach( (char, i) => {
+		const text = new SplitType(char, {types:'chars, words'})
+		
+		gsap.from(text.chars, {
+			scrollTrigger: {
+				trigger: char,
+				scrub: true,
+				start: 'top 35%',
+				end: 'bottom 35%',
 
+			},
+			transformOrigin: 'top',
+			stagger: 0.1,
+			opacity: 0.2,
+	
+		})
 	})
+
+
+	  
+
+}
+
+
+
+window.addEventListener('load', () => {
+	// Initialize a new Lenis instance for smooth scrolling
+	const lenis = new Lenis({
+		smooth: true,
+		lerp: 0.1, // Regola la velocità dello smoothing
+	});
+
+	// Listen for the 'scroll' event and log the event data to the console
+	lenis.on('scroll', (e) => {
+	});
+
+	// Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+	lenis.on('scroll', ScrollTrigger.update);
+
+	// Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+	// This ensures Lenis's smooth scroll animation updates on each GSAP tick
+	gsap.ticker.add((time) => {
+	lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+	});
+
+	// Disable lag smoothing in GSAP to prevent any delay in scroll animations
+	gsap.ticker.lagSmoothing(0);
+
+	init();
+
 })
-
-const lenis = new Lenis()
-
-lenis.on('scroll', (e) => {
-})
-
-lenis.on('scroll', ScrollTrigger.update)
-
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000)
-})
-
-gsap.ticker.lagSmoothing(0)
